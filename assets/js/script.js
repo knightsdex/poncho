7546e27f710e551ca0689f7693c6dab5ef171321
sportsbookdev510 <knightsdex>
2026-02-07 00:20:25 -0500

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 document.getElementById('copyButton').addEventListener('click', function() {
-    const textToCopy = '0x3d2db2bedb736e1f01da133750de4849423a5836';
+    const textToCopy = '0xComingSoon';
     navigator.clipboard.writeText(textToCopy).then(function() {
         const copyMessage = document.getElementById('copyMessage');
         copyMessage.style.display = 'inline';
